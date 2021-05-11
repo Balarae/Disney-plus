900752219be43a221d2edce126b4713eeb9a9e02
Balarae <muhumuzareagan7@gmail.com>
2021-05-11 04:26:00 -0700

    ADD: handled movies from DB

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
+import Detail from './components/Detail'
+import Login from './components/Login';
 
 
 function App() {
   return (
     <div className="App">
-     <Header />
-    <Home />
+     <Router>
+       <Header />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/detail">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+     </Router>
     
     </div>
   );
